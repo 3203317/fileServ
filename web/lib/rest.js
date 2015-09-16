21e855987bd02e818a2cf23d86069ad46add60e8
unknown <3203317@qq.com>
2015-09-16 18:18:30 +0800

    haha

--- web/lib/rest.js
+++ web/lib/rest.js
@@ -8,6 +8,8 @@
 var path = require('path'),
 	cwd = process.cwd();
 
+var crypto = require('crypto');
+
 var util = require('speedt-utils');
 
 var exports = module.exports;
@@ -23,7 +25,10 @@ var exports = module.exports;
  * @return
  */
 exports.genSignature = function(data, seckey){
-	return '987654321';
+	// seckey = 'KFD85H9SmyZd8FSopX_CxxG5VgLFW71LiYc35PxZWXABX9BsANvPUQpLBCrPz25JpSy2_bt2Z0gWRCA6ePsKww';
+	// data = 'apiKey=J4_EFO3ZlBZynJC7dACIFiivoCniAvJlLr-H_dIex-eAdyz1ykGgMtrvcJ7PBCrPKsJRuPaiRKdDuL5LTL_Jag&command=listAccounts&listAll=true&response=json';
+	var signature = crypto.createHmac('sha1', seckey).update(data.toLowerCase()).digest().toString('base64');
+	return encodeURIComponent(signature);
 };
 
 /**
